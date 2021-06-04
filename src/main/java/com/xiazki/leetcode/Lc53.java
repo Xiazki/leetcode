35cb7fa49f68e1ff76e07aae1ca4c289818bcfa0
xiaxiang <xiaxiang3@xdf.cn>
2021-06-04 13:23:26 +0800

    滑动窗口相关练习

---
+++ src/main/java/com/xiazki/leetcode/Lc53.java
@@ -0,0 +1,23 @@
+package com.xiazki.leetcode;
+
+/**
+ * 53. 最大子序和
+ *
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出：6
+ * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+ */
+public class Lc53 {
+
+    public int maxSubArray(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+
+}
