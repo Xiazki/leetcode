8d0ec411c7bfee1caea47e025a12799279435963
xiaxiang <xiaxiang3@xdf.cn>
2021-06-08 13:59:21 +0800

    commit code

---
+++ src/main/java/com/xiazki/leetcode/Lc239.java
@@ -0,0 +1,76 @@
+package com.xiazki.leetcode;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * 滑动窗口最大值
+ * 给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+ * <p>
+ * 返回滑动窗口中的最大值。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+ * 输出：[3,3,5,5,6,7]
+ * 解释：
+ * 滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ * 1 [3  -1  -3] 5  3  6  7       3
+ * 1  3 [-1  -3  5] 3  6  7       5
+ * 1  3  -1 [-3  5  3] 6  7       5
+ * 1  3  -1  -3 [5  3  6] 7       6
+ * 1  3  -1  -3  5 [3  6  7]      7
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1], k = 1
+ * 输出：[1]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [1,-1], k = 1
+ * 输出：[1,-1]
+ * 示例 4：
+ * <p>
+ * 输入：nums = [9,11], k = 2
+ * 输出：[11]
+ * 示例 5：
+ * <p>
+ * 输入：nums = [4,-2], k = 2
+ * 输出：[4]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sliding-window-maximum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Lc239 {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] ret = nums.length <= k ? new int[1] : new int[nums.length - k + 1];
+        for (int i = 0; i < ret.length; i++) {
+            int start = i + 1;
+            ret[i] = nums[i];
+            while (start < nums.length && (start - i) < k) {
+                ret[i] = Math.max(nums[start], ret[i]);
+                start++;
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(Arrays.toString(new Lc239().maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3)));
+//        System.out.println(Arrays.toString(new Lc239().maxSlidingWindow(new int[]{1}, 1)));
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        System.out.println(queue.poll());
+        System.out.println(queue.pop());
+        System.out.println(queue);
+    }
+
+}
