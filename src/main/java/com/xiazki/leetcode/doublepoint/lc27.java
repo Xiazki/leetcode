c18fbdadc4f77f309e9fc668688b4006a65a2ee5
xiaxiang <xiaxianginw@163.com>
2021-06-15 21:12:43 +0800

    commit code

---
+++ src/main/java/com/xiazki/leetcode/doublepoint/lc27.java
@@ -0,0 +1,50 @@
+package com.xiazki.leetcode.doublepoint;
+
+import java.util.Arrays;
+
+/**
+ * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+ * <p>
+ * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+ * <p>
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ * <p>
+ * 输入：nums = [3,2,2,3], val = 3
+ * 输出：2, nums = [2,2]
+ * 解释：函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。你不需要考虑数组中超出新长度后面的元素。例如，函数返回的新长度为 2 ，而 nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案。
+ * <p>
+ *
+ *     [0,1,2,2,3,0,4,2]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-element
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class lc27 {
+
+    public int removeElement(int[] nums, int val) {
+        int lift = 0, right = 0;
+        while (right < nums.length) {
+            if (nums[lift]!=val){
+                lift++;
+            }else if (nums[right] != val) {
+                int temp = nums[lift];
+                nums[lift] = nums[right];
+                nums[right] = temp;
+                lift++;
+            }
+            right++;
+        }
+        return lift;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{2};
+        int len = new lc27().removeElement(array,3);
+        System.out.println(Arrays.toString(array));
+        System.out.println(len);
+        for (int i = 0; i < len; i++) {
+            System.out.print(array[i]+" ");
+        }
+    }
+}
