47d9c78f8f29fe143362c6f44ba6f10db5bcc433
xiazki <xiaxianginw@163.com>
2023-10-08 22:59:28 +0800

    链表相关

--- src/main/java/com/xiazki/leetcode/hot100/LC160相交链表.java
+++ src/main/java/com/xiazki/leetcode/hot100/LC160相交链表.java
@@ -10,6 +10,15 @@ public class LC160相交链表 {
 
 class Solution_LC160 {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pa = headA;
+        ListNode pb = headB;
+        while (pa != pb) {
+            pa = pa == null ? headB : pa.next;
+            pb = pb == null ? headA : pb.next;
+        }
         return null;
     }
 }
